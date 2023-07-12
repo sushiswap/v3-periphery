b7f2707d64ba1d9e0c1488eb69484ca71efecd46
matthewlilley <hello@matthewlilley.com>
2023-07-12 15:04:04 +0100

    chore: init zksync

--- deploy/NFTDescriptor.ts
+++ deploy/NFTDescriptor.ts
@@ -1,24 +1,80 @@
+import { Wallet, utils } from 'zksync-web3'
+import * as ethers from 'ethers'
 import { HardhatRuntimeEnvironment } from 'hardhat/types'
-import { DeployFunction } from 'hardhat-deploy/dist/types'
+import { Deployer } from '@matterlabs/hardhat-zksync-deploy'
 
-const func: DeployFunction = async function ({
-  ethers,
-  getNamedAccounts,
-  deployments,
-  getChainId,
-}: HardhatRuntimeEnvironment) {
-  const { deploy } = deployments
+// An example of a deploy script that will deploy and call a simple contract.
+export default async function (hre: HardhatRuntimeEnvironment) {
+  console.log(`Running deploy script for the Greeter contract`)
 
-  const { deployer } = await getNamedAccounts()
+  // Initialize the wallet.
+  const wallet = new Wallet(process.env.PRIVATE_KEY!)
 
-  await deploy('NFTDescriptor', {
-    from: deployer,
-    args: [],
-    log: true,
-    deterministicDeployment: false,
+  // Create deployer object and load the artifact of the contract you want to deploy.
+  const deployer = new Deployer(hre, wallet)
+  const artifact = await deployer.loadArtifact('contracts/libraries/NFTDescriptor.sol:NFTDescriptor')
+
+  // Estimate contract deployment fee
+  const deploymentFee = await deployer.estimateDeployFee(artifact, [])
+
+  // OPTIONAL: Deposit funds to L2
+  // Comment this block if you already have funds on zkSync.
+  const depositHandle = await deployer.zkWallet.deposit({
+    to: deployer.zkWallet.address,
+    token: utils.ETH_ADDRESS,
+    amount: deploymentFee.mul(2),
   })
+  // Wait until the deposit is processed on zkSync
+  await depositHandle.wait()
+
+  // Deploy this contract. The returned object will be of a `Contract` type, similarly to ones in `ethers`.
+  // `greeting` is an argument for contract constructor.
+  const parsedFee = ethers.utils.formatEther(deploymentFee.toString())
+  console.log(`The deployment is estimated to cost ${parsedFee} ETH`)
+
+  const contract = await deployer.deploy(artifact, [])
+
+  //obtain the Constructor Arguments
+  console.log('constructor args:' + contract.interface.encodeDeploy([]))
+
+  // Show the contract info.
+  const contractAddress = contract.address
+  console.log(`${artifact.contractName} was deployed to ${contractAddress}`)
+
+  // Verify contract programmatically
+  //
+  // Contract MUST be fully qualified name (e.g. path/sourceName:contractName)
+  const contractFullyQualifedName = 'contracts/libraries/NFTDescriptor.sol:NFTDescriptor'
+  const verificationId = await hre.run('verify:verify', {
+    address: contractAddress,
+    contract: contractFullyQualifedName,
+    constructorArguments: [],
+    bytecode: artifact.bytecode,
+  })
+  console.log(`${contractFullyQualifedName} verified! VerificationId: ${verificationId}`)
 }
 
-func.tags = ['NFTDescriptor']
+// import { HardhatRuntimeEnvironment } from 'hardhat/types'
+// import { DeployFunction } from 'hardhat-deploy/dist/types'
 
-export default func
+// const func: DeployFunction = async function ({
+//   ethers,
+//   getNamedAccounts,
+//   deployments,
+//   getChainId,
+// }: HardhatRuntimeEnvironment) {
+//   const { deploy } = deployments
+
+//   const { deployer } = await getNamedAccounts()
+
+//   await deploy('UniswapV3Factory', {
+//     from: deployer,
+//     args: [],
+//     log: true,
+//     deterministicDeployment: false,
+//   })
+// }
+
+// func.tags = ['UniswapV3Factory']
+
+// export default func
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,12 +1,11 @@
 import 'dotenv/config'
 import '@nomiclabs/hardhat-ethers'
-import '@nomiclabs/hardhat-etherscan'
 import '@nomiclabs/hardhat-waffle'
+import '@nomiclabs/hardhat-etherscan'
 import '@nomiclabs/hardhat-ethers'
-import 'hardhat-deploy'
-import 'hardhat-deploy-ethers'
-import 'hardhat-typechain'
-import 'hardhat-watcher'
+import '@matterlabs/hardhat-zksync-deploy'
+import '@matterlabs/hardhat-zksync-solc'
+import '@matterlabs/hardhat-zksync-verify'
 import { task, types } from 'hardhat/config'
 
 const LOW_OPTIMIZER_COMPILER_SETTINGS = {
@@ -58,321 +57,36 @@ const accounts = process.env.PRIVATE_KEY
       accountsBalance: '990000000000000000000',
     }
 
-task('add-fee-tier', 'Add fee tier')
-  .addParam('fee', 'Fee', 100, types.int)
-  .addParam('tickSpacing', 'Tick Spacing', 1, types.int)
-  .setAction(async (taskArgs, hre) => {
-    const { fee, tickSpacing } = taskArgs
-    const { getNamedAccounts, ethers, deployments } = hre
-    const factory = await ethers.getContract('UniswapV3Factory')
-    await factory.enableFeeAmount(fee, tickSpacing)
-    console.log('Enabled fee amount')
-  })
+// task('add-fee-tier', 'Add fee tier')
+//   .addParam('fee', 'Fee', 100, types.int)
+//   .addParam('tickSpacing', 'Tick Spacing', 1, types.int)
+//   .setAction(async (taskArgs, hre) => {
+//     const { fee, tickSpacing } = taskArgs
+//     const { getNamedAccounts, ethers, deployments } = hre
+//     const factory = await ethers.getContract('UniswapV3Factory')
+//     await factory.enableFeeAmount(fee, tickSpacing)
+//     console.log('Enabled fee amount')
+//   })
 
 export default {
+  zksolc: {
+    version: 'latest', // Uses latest available in https://github.com/matter-labs/zksolc-bin/
+    settings: {},
+  },
+  defaultNetwork: 'zksync',
   networks: {
-    arbitrum: {
-      url: 'https://arb1.arbitrum.io/rpc',
-      accounts,
-      chainId: 42161,
-      live: true,
-      saveDeployments: true,
-      blockGasLimit: 700000,
-    },
-    'arbitrum-nova': {
-      url: 'https://nova.arbitrum.io/rpc',
-      accounts,
-      chainId: 42170,
-      live: true,
-      saveDeployments: true,
-    },
-    hardhat: {
-      allowUnlimitedContractSize: false,
-    },
-    ethereum: {
+    mainnet: {
       url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
       accounts,
       chainId: 1,
-      live: true,
-      saveDeployments: true,
     },
-    fantom: {
-      url: 'https://rpcapi.fantom.network',
-      accounts,
-      chainId: 250,
-      live: true,
-      saveDeployments: true,
-    },
-    polygon: {
-      url: 'https://rpc-mainnet.maticvigil.com',
-      accounts,
-      chainId: 137,
-      live: true,
-      saveDeployments: true,
-    },
-    gnosis: {
-      url: 'https://rpc.ankr.com/gnosis',
-      accounts,
-      chainId: 100,
-      live: true,
-      saveDeployments: true,
-    },
-    bsc: {
-      url: 'https://bsc-dataseed.binance.org',
-      accounts,
-      chainId: 56,
-      live: true,
-      saveDeployments: true,
-    },
-    avalanche: {
-      url: 'https://api.avax.network/ext/bc/C/rpc',
-      accounts,
-      chainId: 43114,
-      live: true,
-      saveDeployments: true,
-      // gasPrice: 470000000000,
-    },
-
-    celo: {
-      url: 'https://forno.celo.org',
-      accounts,
-      chainId: 42220,
-      live: true,
-      saveDeployments: true,
-    },
-    moonriver: {
-      url: 'https://rpc.moonriver.moonbeam.network',
-      accounts,
-      chainId: 1285,
-      live: true,
-      saveDeployments: true,
-    },
-    moonbeam: {
-      url: 'https://rpc.api.moonbeam.network',
-      accounts,
-      chainId: 1284,
-      live: true,
-      saveDeployments: true,
-    },
-    fuse: {
-      url: 'https://rpc.fuse.io',
-      accounts,
-      chainId: 122,
-      live: true,
-      saveDeployments: true,
-    },
-    optimism: {
-      url: 'https://mainnet.optimism.io',
-      accounts,
-      chainId: 10,
-      live: true,
-      saveDeployments: true,
-    },
-    kava: {
-      url: 'https://evm.kava.io',
-      accounts,
-      chainId: 2222,
-      live: true,
-      saveDeployments: true,
-    },
-    metis: {
-      url: 'https://andromeda.metis.io/?owner=1088',
-      accounts,
-      chainId: 1088,
-      live: true,
-      saveDeployments: true,
-    },
-    boba: {
-      url: 'https://mainnet.boba.network',
-      accounts,
-      chainId: 288,
-      live: true,
-      saveDeployments: true,
-    },
-    'boba-avax': {
-      url: 'https://avax.boba.network',
-      accounts,
-      chainId: 43288,
-      live: true,
-      saveDeployments: true,
-    },
-    bttc: {
-      url: 'https://rpc.bittorrentchain.io',
-      accounts,
-      chainId: 199,
-      live: true,
-      saveDeployments: true,
-    },
-    'boba-bnb': {
-      url: 'https://bnb.boba.network',
-      accounts,
-      chainId: 56288,
-      live: true,
-      saveDeployments: true,
-    },
-    polygonzkevm: {
-      url: 'https://zkevm-rpc.com',
-      accounts,
-      chainId: 1101,
-      live: true,
-      saveDeployments: true,
-    },
-    thundercore: {
-      url: 'https://mainnet-rpc.thundertoken.net',
-      accounts,
-      chainId: 108,
-      live: true,
-      saveDeployments: true,
-    },
-    filecoin: {
-      url: 'https://rpc.ankr.com/filecoin',
-      accounts,
-      chainId: 314,
-      live: true,
-      saveDeployments: true,
-    },
-    // linea: {
-    //   url: '',
-    //   accounts,
-    //   chainId: ,
-    //   live: true,
-    //   saveDeployments: true,
-    // },
-    haqq: {
-      url: 'https://rpc.eth.haqq.network',
-      accounts,
-      chainId: 11235,
-      live: true,
-      saveDeployments: true,
-    },
-    core: {
-      url: 'https://rpc.coredao.org',
-      accounts,
-      chainId: 1116,
-      live: true,
-      saveDeployments: true,
-    },
-    // ropsten: {
-    //   url: `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`,
-    // },
-    // rinkeby: {
-    //   url: `https://rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`,
-    // },
-    // goerli: {
-    //   url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
-    // },
-    // kovan: {
-    //   url: `https://kovan.infura.io/v3/${process.env.INFURA_API_KEY}`,
-    // },
-    // arbitrumRinkeby: {
-    //   url: `https://arbitrum-rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`,
-    // },
-    // arbitrum: {
-    //   url: `https://arbitrum-mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
-    // },
-    // optimismKovan: {
-    //   url: `https://optimism-kovan.infura.io/v3/${process.env.INFURA_API_KEY}`,
-    // },
-    // optimism: {
-    //   url: `https://optimism-mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
-    // },
-    // mumbai: {
-    //   url: `https://polygon-mumbai.infura.io/v3/${process.env.INFURA_API_KEY}`,
-    // },
-    // polygon: {
-    //   url: `https://polygon-mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
-    // },
-    // bnb: {
-    //   url: `https://bsc-dataseed.binance.org/`,
-    // },
-  },
-  namedAccounts: {
-    // e.g. ledger://0x18dd4e0Eb8699eA4fee238dE41ecF115e32272F8
-    deployer: process.env.LEDGER || { default: 0 },
-    alice: {
-      default: 1,
-    },
-    bob: {
-      default: 2,
-    },
-    carol: {
-      default: 3,
-    },
-    dev: {
-      default: 4,
-    },
-    feeTo: {
-      default: 5,
+    zksync: {
+      url: 'https://mainnet.era.zksync.io',
+      ethNetwork: 'mainnet', // RPC URL of the network (e.g. `https://goerli.infura.io/v3/<API_KEY>`)
+      zksync: true,
+      verifyURL: 'https://explorer.zksync.io/contract_verification', // Verification endpoint
     },
   },
-  etherscan: {
-    apiKey: 'ZVIUC4QGHCJWGZ51YU4XIM1PR9KQ4YTW5Z',
-  },
-  // etherscan: {
-  //   customChains: [
-  //     {
-  //       network: 'kava',
-  //       chainId: 2222,
-  //       urls: {
-  //         apiURL: 'https://explorer.kava.io/api',
-  //         browserURL: 'https://explorer.kava.io',
-  //       },
-  //     },
-  //     {
-  //       network: 'metis',
-  //       chainId: 1088,
-  //       urls: {
-  //         apiURL: 'https://andromeda-explorer.metis.io/api',
-  //         browserURL: 'https://andromeda-explorer.metis.io',
-  //       },
-  //     },
-  //   ],
-  //   apiKey: {
-  //     mainnet: process.env.ETHERSCAN_API_KEY || '',
-  //     ropsten: process.env.ETHERSCAN_API_KEY || '',
-  //     rinkeby: process.env.ETHERSCAN_API_KEY || '',
-  //     goerli: process.env.ETHERSCAN_API_KEY || '',
-  //     kovan: process.env.ETHERSCAN_API_KEY || '',
-  //     // binance smart chain
-  //     bsc: process.env.BSCSCAN_API_KEY || '',
-  //     bscTestnet: process.env.BSCSCAN_API_KEY || '',
-  //     // huobi eco chain
-  //     heco: process.env.HECOINFO_API_KEY || '',
-  //     hecoTestnet: process.env.HECOINFO_API_KEY || '',
-  //     // fantom mainnet
-  //     opera: process.env.FTMSCAN_API_KEY || '',
-  //     ftmTestnet: process.env.FTMSCAN_API_KEY || '',
-  //     // optimism
-  //     optimisticEthereum: process.env.OPTIMISTIC_ETHERSCAN_API_KEY || '',
-  //     optimisticKovan: process.env.OPTIMISTIC_ETHERSCAN_API_KEY || '',
-  //     // polygon
-  //     polygon: process.env.POLYGONSCAN_API_KEY || '',
-  //     polygonMumbai: process.env.POLYGONSCAN_API_KEY || '',
-  //     // arbitrum
-  //     arbitrumOne: process.env.ARBISCAN_API_KEY || '',
-  //     arbitrumTestnet: process.env.ARBISCAN_API_KEY || '',
-  //     // avalanche
-  //     avalanche: process.env.SNOWTRACE_API_KEY || '',
-  //     avalancheFujiTestnet: process.env.SNOWTRACE_API_KEY || '',
-  //     // moonbeam
-  //     moonbeam: process.env.MOONBEAM_MOONSCAN_API_KEY || '',
-  //     moonriver: process.env.MOONRIVER_MOONSCAN_API_KEY || '',
-  //     moonbaseAlpha: process.env.MOONBASE_MOONSCAN_API_KEY || '',
-  //     // harmony
-  //     harmony: process.env.HARMONY_API_KEY || '',
-  //     harmonyTest: process.env.HARMONY_API_KEY || '',
-  //     // xdai and sokol don't need an API key, but you still need
-  //     // to specify one; any string placeholder will work
-  //     xdai: 'api-key',
-  //     sokol: 'api-key',
-  //     aurora: 'api-key',
-  //     auroraTestnet: 'api-key',
-  //     metis: 'api-key',
-  //     // bobaAvax: 'api-key',
-  //     bttc: process.env.BTTC_API_KEY || '',
-  //     gnosis: process.env.GNOSIS_API_KEY || '',
-  //   },
-  // },
   solidity: {
     compilers: [DEFAULT_COMPILER_SETTINGS],
     overrides: {
