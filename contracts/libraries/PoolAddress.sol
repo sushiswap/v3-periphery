faac279f82997336b4fe0fd875a4724cb851b1bf
0xMasayoshi <0xMasayoshi@protonmail.com>
2024-03-01 17:08:31 +0700

    feat: blast

--- contracts/libraries/PoolAddress.sol
+++ contracts/libraries/PoolAddress.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.5.0;
 
 /// @title Provides functions for deriving a pool address from the factory, tokens, and the fee
 library PoolAddress {
-    bytes32 internal constant POOL_INIT_CODE_HASH = 0xe34f199b19b2b4f47f68442619d555527d244f78a3297ea89325f843f87b8b54;
+    bytes32 internal constant POOL_INIT_CODE_HASH = 0x8e13daee7f5a62e37e71bf852bcd44e7d16b90617ed2b17c24c2ee62411c5bae;
 
     /// @notice The identifying key of the pool
     struct PoolKey {
